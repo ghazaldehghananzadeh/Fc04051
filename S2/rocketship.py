872317cb0e521a5c749a06da6cc0a83c2dbf1875
GhazalDz <ghazaldehghananzadeh@gmail.com>
2025-11-09 20:32:07 +0330

    tamrin2

---
+++ S2/rocketship.py
@@ -0,0 +1,18 @@
+
+def Head():
+    print("")
+    print("     ^      ")
+    print("    /|\     ")
+    print("   //|\\    ")
+    print("  ///|\\\   ")
+
+
+def Line():
+    print(" +-------+  ")
+
+
+def Body():
+    print(" +*******+  ")
+    print(" +*******+  ")
+    print(" +*******+  		")
+    print(" +*******+  ")
